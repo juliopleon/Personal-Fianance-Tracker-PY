8a0c2cfae010846bf927a901cfd56d1f7216d1a6
Julio Leon <juliopena143@icloud.com>
2024-10-23 15:02:39 -0600

    class methods

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import csv
 from datetime import datetime
+from data_entry import get_amount, get_category, get_date, get_description
 
 
 class CSV:
     CSV_FILE = "finance_data.csv"
     COLUMNS = ["date", "amount", "category", "description"]
+    FORMAT = "%d-%m-%Y"
 
     @classmethod
     def initialize_csv(cls):
@@ -29,6 +31,22 @@ class CSV:
             writer.writerow(new_entry)
         print("Entry added successfully")
 
+    @classmethod
+    def get_transactions(cls, start_date, end_date):
+        df = pd.read_csv(cls.CSV_FILE)
+        df["date"] = pd.to_datetime(df["date"], format=CSV.FORMAT)
+        start_date = datetime.strftime(start_date, CSV.FORMAT)
+        end_date = datetime.strftime(end_date, CSV.FORMAT)
 
-CSV.initialize_csv()
-CSV.add_entry("20-10-2024", 123.65, "Income", "Salary")
+
+def add():
+    CSV.initialize_csv()
+    date = get_date(
+        "Enter the date of the transaction (dd-mm-yyyy) or click enter for today's date: ", allow_default=True)
+    amount = get_amount()
+    category = get_category()
+    description = get_description()
+    CSV.add_entry(date, amount, category, description)
+
+
+add()
