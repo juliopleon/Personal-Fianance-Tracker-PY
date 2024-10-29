3799ecbf3f28651f031cd3411bb8a037c676a99f
Julio Leon <juliopena143@icloud.com>
2024-10-29 15:40:59 -0600

    print

--- main.py
+++ main.py
@@ -42,7 +42,21 @@ class CSV:
         filtered_df = df.loc[mask]
 
         if filtered_df.empty:
-            print('No transactions found in the given date range')
+            print("No transactions found in the given date range")
+        else:
+            print(
+                f"Transactions from {start_date.strftime(CSV.FORMAT)} to {end_date.strftime(CSV.FORMAT)}")
+            print(filtered_df.to_string(index=False, formatters={
+                  "date": lambda x: x.strftime(CSV.FORMAT)}))
+
+            total_income = filtered_df[filtered_df["category"]
+                                       == "Income"]["amount"].sum()
+            total_expense = filtered_df[filtered_df["category"]
+                                        == "Expense"]["amount"].sum()
+            print("\nSummary:")
+            print(f"Total Income: ${total_income:.2f}")
+            print(f"Total Expense: ${total_expense:.2f}")
+            print(f"Net Savings: ${(total_income - total_expense):.2f}")
 
 
 def add():
