0c3a903ed00e99fe6fedb0f3294903cf6e4b40a5
Julio Leon <juliopena143@icloud.com>
2024-11-01 15:09:21 -0600

    df pandas

--- main.py
+++ main.py
@@ -71,7 +71,42 @@ def add():
     CSV.add_entry(date, amount, category, description)
 
 
+def plot_transactions(df):
+    df.set_index("date", inplace=True)
+
+    income_df = (
+        df[df["category"] == "Income"]
+        .resample("D")
+        .sum()
+        .reindex(df.index, fill_value=0))
+
+    expense_df = (
+        df[df["category"] == "Expense"]
+        .resample("D")
+        .sum()
+        .reindex(df.index, fill_value=0))
+
+
 def main():
     while True:
         print("\n1. Add a new transaction")
-        print("\n.2 View Transactions and a summary within a date range")
+        print("2. View Transactions and a summary within a date range")
+        print("3. Exit")
+        choice = input("Enter your choice (1-3): ")
+
+        if choice == "1":
+            add()
+        elif choice == "3":
+            start_date = get_date("Enter the start date (dd-mm-yyyy): ")
+            end_date = get_date("Enter the end date (dd-mm-yyyy): ")
+            df = CSV.get_transactions(start_date, end_date)
+        elif choice == "3":
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice. Enter 1, 2 or 3.")
+
+
+# only execute if main is called for execution
+if __name__ == "__main__":
+    main()
