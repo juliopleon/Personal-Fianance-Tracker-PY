ba97c89f31bf0334c4c2384e0d5cf1bb0b2c9b01
Julio Leon <juliopena143@icloud.com>
2024-11-04 16:27:55 -0700

    plot finished

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import csv
 from datetime import datetime
 from data_entry import get_amount, get_category, get_date, get_description
+import matplotlib.pyplot as plt
 
 
 class CSV:
@@ -86,6 +87,20 @@ def plot_transactions(df):
         .sum()
         .reindex(df.index, fill_value=0))
 
+    plt.figure(figsize=(10, 5))
+    # first param is x axis second is Y axis
+    plt.plot(income_df.index, income_df["amount"], label="Income", color="g")
+    plt.plot(expense_df.index,
+             expense_df["amount"], label="Expense", color="r")
+    plt.xlabel("Date")
+    plt.ylabel("Amount")
+    plt.title('Income and Expenses for Provided Dates')
+    # enables colored labels for income and expense
+    plt.legend()
+    # enables grid lines
+    plt.grid(True)
+    plt.show()
+
 
 def main():
     while True:
@@ -100,6 +115,8 @@ def main():
             start_date = get_date("Enter the start date (dd-mm-yyyy): ")
             end_date = get_date("Enter the end date (dd-mm-yyyy): ")
             df = CSV.get_transactions(start_date, end_date)
+            if input("Do you want to see a plot? (y/n)").lower() == "y":
+                plot_transactions(df)
         elif choice == "3":
             print("Exiting...")
             break
